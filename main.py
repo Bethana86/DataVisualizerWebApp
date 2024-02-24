8b0f6bc9bcd86215e8cb56aee313f8212fcd8659
Bethanasamy Rajamani <46414051+Bethana86@users.noreply.github.com>
2024-02-24 19:24:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -76,14 +76,11 @@ if selected_file:
 
         # Show the results
         st.pyplot(fig)
-
-    #hide
-        
-    hide_st_style = """
+hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
             """
-    st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True) 
